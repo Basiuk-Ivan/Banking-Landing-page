4744cc7929e74c0b6ac1f9005ccc53702e3034fb
IvanBasss <122455382+Basiuk-Ivan@users.noreply.github.com>
2023-08-27 14:45:26 +0300

    create landing page

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from './Navbar';
+import Billing from './Billing';
+import CartDeal from './CartDeal';
+import Business from './Business';
+import Clients from './Clients';
+import CTA from './CTA';
+import Stats from './Stats';
+import Footer from './Footer';
+import Testimonials from './Testimonials';
+import Hero from './Hero';
+
+export {
+  Navbar,
+  Billing,
+  CartDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
